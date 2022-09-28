3bddd6976a9594418fb53c5da821ed45614b9f12
M Azhar Alauddin <80077273+zharmedia386@users.noreply.github.com>
2022-09-28 09:30:55 +0700

    cursor style & animation

--- Mission4_Geopark/assets/js/app.js
+++ Mission4_Geopark/assets/js/app.js
@@ -31,3 +31,31 @@ sr.reveal('.first-paragraph',{origin: 'top', interval: 200});
 // sr.reveal('.content',{origin: 'left', delay: 600, interval: 200}); 
 
 
+
+// CURSOR ANIMATION
+const cursor = document.querySelector(".cursor");
+var timeout;
+
+//follow cursor on mousemove
+document.addEventListener("mousemove", (e) => {
+    let x = e.pageX;
+    let y = e.pageY;
+
+    cursor.style.top = y + "px";
+    cursor.style.left = x + "px";
+    cursor.style.display = "block";
+
+    //cursor effects when mouse stopped
+    function mouseStopped(){
+    cursor.style.display = "none";
+    }
+    clearTimeout(timeout);
+    timeout = setTimeout(mouseStopped, 1000);
+});
+
+//cursor effects when mouseout
+document.addEventListener("mouseout", () => {
+    cursor.style.display = "none";
+});
+
+
